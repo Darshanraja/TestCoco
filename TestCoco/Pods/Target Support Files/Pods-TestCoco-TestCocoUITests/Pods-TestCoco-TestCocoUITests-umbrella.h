0f7115469b25f4a3b7de010d9904d8ea66fcc17a
Darshanraja <148184582+Darshanraja@users.noreply.github.com>
2024-09-30 08:20:53 -0400

    Add files via upload

---
+++ TestCoco/Pods/Target Support Files/Pods-TestCoco-TestCocoUITests/Pods-TestCoco-TestCocoUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TestCoco_TestCocoUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TestCoco_TestCocoUITestsVersionString[];
+
---
+++ TestCoco/Pods/Target Support Files/Pods-TestCoco/Pods-TestCoco-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestCoco : NSObject
+@end
+@implementation PodsDummy_Pods_TestCoco
+@end
